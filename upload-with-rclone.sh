4afa5ff270fa934d8c80c676422a5d346c875c39
BluABK <BluABK@users.noreply.github.com>
2020-07-14 20:31:13 +0200

    Added handling of invalid dest dir

--- upload-with-rclone.sh
+++ upload-with-rclone.sh
@@ -6,19 +6,19 @@ if [ $# -eq 0 ]; then
         exit 1
 fi
 
-
 # FIXME: Config opts.
 default_remote="tohru:"
-# rclone_opts="-v"
-# rclone_copy_opts="--progress"
+remote_mount_path="/tohru"
 dialog_util=$(which yad)
 
 # Essentials.
 source_path="$1"
-echo "source path: $source_path"
+readarray -d '/' -t remote_mount_path_arr < <( echo "$remote_mount_path"  )
 dialog=$(which $dialog_util)
 rclone=$(which rclone)
 dialog_title="Upload with RClone..."
+
+echo "source path: $source_path"
 
 # Get list of remotes.
 readarray -t remotes < <( $rclone listremotes )
@@ -61,10 +61,10 @@ echo "dialog1: $dialog1"
 # Determine selected input from returned list.
 readarray -d '|' -t dialog1_arr < <( echo "$dialog1"  )
 
+
 # Set variables for selection.
 sel_remote=${dialog1_arr[0]}
 sel_transfers=${dialog1_arr[1]}
-# if ["${dialog1_arr[2]}" == "$source_path"]
 sel_dest=${dialog1_arr[2]}
 # Label field on index 3
 sel_rclone_opt_noverbose=${dialog1_arr[4]}
@@ -82,3 +82,27 @@ echo -e "\tNOT Verbose:  $sel_rclone_opt_noverbose"
 echo -e "\tNOT Progress: $sel_rclone_copy_opt_noprogress"
 echo -e "\tNOT incl dir: $sel_rclone_copy_opt_nodir"
 echo -e "\tNOT SSH:      $sel_opt_nossh"
+
+# Determine dest root:
+# Split dest on '/'.
+readarray -d '/' -t dest_arr_messy < <( echo "$sel_dest"  )
+# # Remove all the useless empty string entries produced by above split.
+# dest_arr=()
+# for item in "${dest_arr_messy[@]}"
+# do
+#     if ["$item" != '']; then
+#         dest_arr+=("$item")
+#     fi
+# done
+
+# # Iteratate remote_mount_path_arr against dest_arr_messy to make sure it's a match.
+# for part in "${remote_mount_path_arr[@]}"
+# do
+#     if ["$part" == "${dest_arr_messy[i]}"]; then
+# done
+
+if [[ "$sel_dest" == "$remote_mount_path"* ]]; then
+    echo "Do stuff"
+else
+    $dialog --image dialog-error --center --title "Error" --text "Destination path does not start with RClone mount path!\n\nMount: $remote_mount_path\nDest: $sel_dest"
+fi
